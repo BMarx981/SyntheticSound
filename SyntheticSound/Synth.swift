5c30cbd6200f5ac8f82c9f3c46dbca5c413c7b21
Brian Marx <b.marx981@gmail.com>
2016-12-18 20:20:51 -0500

    Initial Commit. Every filter is sent through the mixer and the selector turns the volume up or down.

---
+++ SyntheticSound/Synth.swift
@@ -0,0 +1,67 @@
+//
+//  Synth.swift
+//  SyntheticSound
+//
+//  Created by Brian Marx on 12/18/16.
+//  Copyright © 2016 Brian Marx. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class Synth {
+    
+    var mainOsc = AKOscillator(waveform: AKTable(.sawtooth))
+    var lp = AKLowPassFilter(AKOscillator(), cutoffFrequency: 1000.0)
+    var bp = AKBandPassFilter(AKOscillator(), centerFrequency: 1000.0)
+    var hp = AKHighPassFilter(AKOscillator(), cutoffFrequency: 1000.0)
+    var none = AKHighShelfFilter(AKOscillator(), cutOffFrequency: 20000)
+    
+    var mixer = AKMixer()
+    
+    var mainFilterFreq = 1000.0
+    
+    init() {
+        mixer = AKMixer(lp, bp, hp, none)
+        mixer.start()
+    }
+    
+    func setOsc(osc: String) {
+        switch osc {
+            case "saw": mainOsc = AKOscillator(waveform: AKTable(.sawtooth))
+            case "square": mainOsc = AKOscillator(waveform: AKTable(.square))
+            case "sine": mainOsc = AKOscillator(waveform: AKTable(.sine))
+            case "triangle": mainOsc = AKOscillator(waveform: AKTable(.triangle))
+            default:
+                break;
+        }
+    }//end setOsc
+    
+    func setFilter(filter: String) {
+        switch filter {
+            case "lp": lp = AKLowPassFilter(mainOsc, cutoffFrequency: mainFilterFreq)
+                lp.dryWetMix = 100
+                bp.dryWetMix = 0
+                hp.dryWetMix = 0
+                none.dryWetMix = 0
+            case "bp": bp = AKBandPassFilter(mainOsc, centerFrequency: mainFilterFreq)
+                bp.dryWetMix = 100
+                lp.dryWetMix = 0
+                hp.dryWetMix = 0
+                none.dryWetMix = 0
+            case "hp": hp = AKHighPassFilter(mainOsc, cutoffFrequency: mainFilterFreq)
+                hp.dryWetMix = 100
+                bp.dryWetMix = 0
+                lp.dryWetMix = 0
+                none.dryWetMix = 0
+            case "none": none = AKHighShelfFilter(mainOsc, cutOffFrequency: 20000.0)
+                none.dryWetMix = 100
+                bp.dryWetMix = 0
+                hp.dryWetMix = 0
+                lp.dryWetMix = 0
+            default: break
+        }
+    }//end setFilter
+
+    
+}
--- SyntheticSound/ViewController.swift
+++ SyntheticSound/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //Play Stop button controls synth activation
+    @IBAction func PlayStop(_ sender: UIButton) {
+    }
+    
+    //OscSelector chooses which oscillator is used
+    @IBAction func OscSelector(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0: setOsc(osc: "saw")
+            case 1: setOsc(osc: "square")
+            case 2: setOsc(osc: "sine")
+            case 3: setOsc(osc: "triangle")
+            default: break
+        }
+    }
+    
+    //Oscillator frequency slider
+    @IBAction func OscFreqSlider(_ sender: UISlider) {
+        setOscFreq(10e(Double(sender.value))
+    }
+    
+    //Filter Selector Control chooses which filter is used
+    @IBAction func FilterSelector(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0: setFilter(filter: "lp")
+            case 1: setFilter(filter: "bp")
+            case 2: setFilter(filter: "hp")
+            case 3: setFilter(filter: "none")
+        }
+    }
 
+    //Filter Frequency Slider
+    @IBAction func FilterFreqSlider(_ sender: UISlider) {
+        setFilterFreq(10e(Double(sender.value))
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
